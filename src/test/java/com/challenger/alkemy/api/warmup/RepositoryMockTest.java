768f9bb48a81d98a942ea098a465d01dd21cb984
NAHUEL <novando@tekgenesis.com>
2021-06-17 17:41:06 -0300

    Test, only test repository

--- src/test/java/com/challenger/alkemy/api/warmup/RepositoryMockTest.java
+++ src/test/java/com/challenger/alkemy/api/warmup/RepositoryMockTest.java
@@ -1,37 +1,30 @@
 package com.challenger.alkemy.api.warmup;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 
-import com.challenger.alkemy.api.warmup.commons.CommonsUtils;
-import com.challenger.alkemy.api.warmup.controllers.PostController;
 import com.challenger.alkemy.api.warmup.models.entity.Categoria;
 import com.challenger.alkemy.api.warmup.models.entity.Post;
 import com.challenger.alkemy.api.warmup.models.entity.Usuario;
 import com.challenger.alkemy.api.warmup.models.repository.CategoriaRepository;
 import com.challenger.alkemy.api.warmup.models.repository.PostRepository;
 import com.challenger.alkemy.api.warmup.models.repository.UsuarioRepository;
-import com.challenger.alkemy.api.warmup.services.PostService;
 
-class controllerTest {
 
-	
+class RepositoryMockTest {
 	@Autowired
     Optional<Post> post;
-	
-	@Autowired
-	CommonsUtils commonsUtil;
-	
-	@Autowired
-	PostService postService;
 
     PostRepository postRepositoryMock = Mockito.mock(PostRepository.class);
     
@@ -39,18 +32,16 @@ class controllerTest {
 
     UsuarioRepository usuarioRepositoryMock = Mockito.mock(UsuarioRepository.class);
     
-    
-
-    @Autowired
-    PostController postController = new PostController();
 	
 	@BeforeEach
-	void setUp() {
-		System.out.println("aca es before");
-		
+	void setUp() {				
 		Categoria mockCategoria = new Categoria();
 		mockCategoria.setId(Long.valueOf(1));
 		mockCategoria.setDescripcion("Matematica");
+		
+		Categoria mockCategoria2 = new Categoria();
+		mockCategoria2.setId(Long.valueOf(2));
+		mockCategoria2.setDescripcion("Historia");
 		
 		Usuario mockUsuario = new Usuario();
 		mockUsuario.setId(Long.valueOf(1));
@@ -65,27 +56,61 @@ class controllerTest {
 		mockPost.setCategoria(mockCategoria);
 		mockPost.setUsuario(mockUsuario);
 		
+		Post mockPost2 = new Post();
+		
+		mockPost2.setTitulo("Matematica Moderna 2");
+		mockPost2.setContenido("Matematica America");
+		mockPost2.setImagen("https://image.freepik.com/vector-gratis/ilustracion-dia-internacional-jazz-instrumentos-musicales_23-2148865774.jpg");
+		mockPost2.setCategoria(mockCategoria);
+		mockPost2.setUsuario(mockUsuario);
+		
+		Post mockPost3 = new Post();
+		
+		mockPost2.setTitulo("Historia Americana");
+		mockPost2.setContenido("Historia Americana");
+		mockPost2.setImagen("https://image.freepik.com/vector-gratis/ilustracion-dia-internacional-jazz-instrumentos-musicales_23-2148865774.jpg");
+		mockPost2.setCategoria(mockCategoria2);
+		mockPost2.setUsuario(mockUsuario);
+		
         Mockito.when(postRepositoryMock.findById(Long.valueOf(1))).thenReturn(Optional.of(mockPost));
+        
+        List<Post> mockPostList = new ArrayList<>();
+
+        
+        mockPostList.add(mockPost);
+        mockPostList.add(mockPost2);
+        mockPostList.add(mockPost3);
+
+
+        Mockito.when(postRepositoryMock.findAll()).thenReturn(mockPostList);
 
 	}
 	
 	@Test
 	void getPostWithValidPostCode() {
-        ResponseEntity<Post> respuestaServicio;
         Post postFound = postRepositoryMock.findById(Long.valueOf(1)).get();
-        Assertions.assertEquals("Matematica Modernar",postFound.getTitulo());
+        Assertions.assertEquals("Matematica Moderna",postFound.getTitulo());
     }
+	
+	
+	@Test
+	public void getPostAllSuccess() throws Exception {
+		 List<Post> postAll = (List<Post>) postRepositoryMock.findAll();
+         assertEquals(3, postAll.size());
+	}
+	
+	@Test
+	public void getPostAllFail() throws Exception {
+		 List<Post> postAll = (List<Post>) postRepositoryMock.findAll();
+         assertFalse(postAll.size()==5);
+	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-		System.out.println("aca es after");
-
 	}
 
 	@Test
 	void test() {
-		System.out.println("aca es test principal");
-
 	}
 
 }
